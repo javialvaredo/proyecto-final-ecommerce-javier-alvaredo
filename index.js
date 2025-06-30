dc4b0357a083eacac61d1186181dee0994c056ba
javier Alvaredo <javi.alvaredo@gmail.com>
2025-06-30 19:15:32 -0300

    agregado funcion para servir pagina estatica en carpeta public

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import express from "express";
 import cors from "cors";
+import {join, dirname} from "path";
+import { fileURLToPath } from "url";
 
 import productsRouter from "./src/routes/products.routes.js";
 import usersRouter from "./src/routes/users.routes.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const PORT = 3000;
@@ -13,12 +18,15 @@ app.use(cors());
 // Middleware para parsear JSON
 app.use(express.json()); 
 
+//Middleware para servir archivos estáticos 
+app.use(express.static(join(__dirname, 'public')));
+
 //aqui van las rutas
 app.use('/api', productsRouter);
 
 app.use('/api', usersRouter);
 
-
+console.log("Sirviendo archivos estáticos desde:", join(__dirname, 'public'));
 
 // Middleware para manejar errores 404 
 app.use((req, res, next) => { 
